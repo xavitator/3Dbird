c0dc954474261f55239b8600e07e97992e8d9fb1
ThoLes94 <59646128+ThoLes94@users.noreply.github.com>
2021-05-21 11:09:11 +0200

    Scene finie avec texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
+#define _USE_MATH_DEFINES
+
 #include "vcl/vcl.hpp"
 #include <iostream>
-
+#include <random>
 #include "terrain.hpp"
 #include "tree.hpp"
+#include <math.h>
 
 using namespace vcl;
+
 
 struct gui_parameters {
 	bool display_frame = true;
@@ -37,13 +41,40 @@ void display_scene();
 void display_interface();
 
 mesh terrain;
+mesh ocean_m;
 mesh_drawable terrain_visual;
+mesh_drawable ocean;
 mesh_drawable tree;
 mesh_drawable feuille;
+mesh_drawable wall;
+mesh_drawable caillou;
+mesh_drawable cloud1;
+mesh_drawable cloud2;
+mesh_drawable cloud3;
+mesh_drawable cloud4;
+mesh_drawable ship;
+mesh_drawable ring;
+
+
 std::vector<vcl::vec3> tree_position;
 perlin_noise_parameters parameters;
 mesh_drawable billboard_grass;
 mesh_drawable billboard_bird;
+std::vector<mesh_drawable> liste_iles;
+std::vector<vcl::vec3> ile_position;
+std::vector<std::vector<vcl::vec3>> liste_tree_position;
+std::vector<float> ile_orientation;
+std::vector<vcl::vec3> cloud_position;
+std::vector<float> cloud_orientation;
+std::vector<vcl::vec3> ship_position;
+std::vector<float> ship_orientation;
+
+int taille_terrain = 100;
+int nb_iles = 5;
+int nb_arbres = 20;
+int nb_cloud = 40;
+int nb_ship = 20;
+
 
 int main(int, char* argv[])
 {
@@ -100,7 +131,23 @@ int main(int, char* argv[])
 	return 0;
 }
 
+mesh_drawable ile(perlin_noise_parameters par2) {
+	terrain = create_terrain(par2);
+	mesh_drawable terrain_visual1 = mesh_drawable(terrain);
+	//terrain_visual1.shading.color = { 0.6f,0.85f,0.5f };
+	terrain_visual1.shading.phong.specular = 0.0f; // non-specular terrain material
 
+	image_raw const im2 = image_load_png("assets/sandgrass.png");
+
+	// Send this image to the GPU, and get its identifier texture_image_id
+	GLuint const texture_image_id2 = opengl_texture_to_gpu(im2,
+		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_S*/,
+		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_T*/);
+
+	terrain_visual1.texture = texture_image_id2;
+
+	return terrain_visual1;
+}
 
 void initialize_data()
 {
@@ -118,39 +165,140 @@ void initialize_data()
 	scene.camera.look_at({4,3,2}, {0,0,0}, {0,0,1});
 
     // Create visual terrain surface
-	terrain = create_terrain(parameters);
-	terrain_visual = mesh_drawable(terrain);
-    terrain_visual.shading.color = {0.6f,0.85f,0.5f};
-    terrain_visual.shading.phong.specular = 0.0f; // non-specular terrain material
-
-	image_raw const im2 = image_load_png("assets/texture_grass.png");
+	tree = mesh_drawable(create_tree());
+	
+	
+	ile_position = generate_positions_ile(nb_iles,taille_terrain);
+	ile_orientation = generate_rotation(5);
+	for (int k = 0; k < 5; k++) {
+		perlin_noise_parameters par2= generate_alea_ile();
+		liste_iles.push_back(ile(par2));
+		liste_tree_position.push_back(generate_positions_on_terrain(nb_arbres, par2));
+	}
+	wall = mesh_drawable(create_wall(taille_terrain));
+	image_raw const im3 = image_load_png("assets/rayure1.png");
 
 	// Send this image to the GPU, and get its identifier texture_image_id
-	GLuint const texture_image_id2 = opengl_texture_to_gpu(im2,
+	GLuint const texture_image_id3 = opengl_texture_to_gpu(im3,
 		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_S*/,
 		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_T*/);
 
-	terrain_visual.texture = texture_image_id2;
+	wall.texture = texture_image_id3;
+	wall.shading.alpha = 0.5f;
+
+	caillou = mesh_drawable(mesh_load_file_obj("assets/caillou.obj"));
+	caillou.shading.color={0.63f,0.63f,0.63f};
+	caillou.transform.scale = 100;
+	caillou.transform.rotate = rotation({ 1,0,0 }, 0.5f * M_PI);
+
+	cloud1 = mesh_drawable(mesh_load_file_obj("assets/Cloud_1.obj"));
+	cloud2 = mesh_drawable(mesh_load_file_obj("assets/Cloud_2.obj"));
+	cloud3 = mesh_drawable(mesh_load_file_obj("assets/Cloud_3.obj"));
+	cloud4 = mesh_drawable(mesh_load_file_obj("assets/Cloud_4.obj"));
+
+	cloud_position = generate_positions_clouds(nb_cloud, taille_terrain);
+	cloud_orientation = generate_rotation_cloud(20);
 	
-
-
-	feuille = mesh_drawable(create_cone(1.0f, 2.0f, 2.0f));
-	feuille.shading.color = { 0.0f, 1.0f, 0.0f };
-
-	tree = mesh_drawable(create_tree());
+	ship = mesh_drawable(mesh_load_file_obj("assets/ship.obj"));
+	ship.transform.scale = 0.01f;
+	ship.shading.color = { 0.24f,0.20f,0.2f };
 	
+	ring = mesh_drawable(mesh_load_file_obj("assets/cercle.obj"));
 
-	tree_position = generate_positions_on_terrain(100, parameters);
+	ship_position = generate_positions_ships(nb_ship, taille_terrain, ile_position);
+	ship_orientation = generate_rotation_ship(nb_ship);
+
+	ocean_m = create_ocean(parameters, taille_terrain);
+	ocean = mesh_drawable(ocean_m);
+	//ocean.shading.color = { 0.0f, 0.0f, 1.0f };
+	image_raw const im4 = image_load_png("assets/ocean.png");
+	GLuint const texture_image_id4 = opengl_texture_to_gpu(im4,
+		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_S*/,
+		GL_MIRRORED_REPEAT /**GL_TEXTURE_WRAP_T*/);
+
+	ocean.texture = texture_image_id4;
+
 }
 
 
 void display_scene()
-{
-	draw(terrain_visual, scene);
-	for (int i = 0; i < 100; i++) {
-		tree.transform.translate = tree_position[i];
-		draw(tree, scene);
+{	
+	update_ocean(ocean_m, ocean, parameters);
+	for (int k = 0; k < nb_iles; k++) {
+		liste_iles[k].transform.translate = ile_position[k];
+		//liste_iles[k].transform.rotate = rotation({ 0,0,1 }, ile_orientation[k]);
+		draw(liste_iles[k], scene);
+		int j = 0;
+		for (int i = 0; i < nb_arbres; i++) {
+			if (j < 2) {
+				tree.transform.translate = liste_tree_position[k][i];
+				tree.transform.translate += ile_position[k];
+				draw(tree, scene);
+				//ocean.transform.translate = { 0,0,10*i };
+				//draw(ocean, scene);
+				j++;
+			}
+			else
+			{
+				j = 0;
+				caillou.transform.translate = liste_tree_position[k][i];
+				caillou.transform.translate += ile_position[k] - vec3({0, 0, 0.1f});
+				draw(caillou, scene);
+			}
+		}
 	}
+	for (int k = 0; k< nb_ship; k++) {
+		ship.transform.translate = ship_position[k];
+		ship.transform.translate += {0, 0, ocean_height(ship_position[k][0]+taille_terrain/2, ship_position[k][1] + taille_terrain / 2, taille_terrain, parameters, 0.2f) * 0.8f};
+		ship.transform.rotate = rotation({ { cos(ship_orientation[k]),0,sin(ship_orientation[k]) }, { sin(ship_orientation[k]),0,-cos(ship_orientation[k]) },{0,1,0} });
+		//ship.transform.rotate = rotation({ 1,0,0 }, 0.5f * M_PI);
+		draw(ship, scene);
+	}
+	for (int i = 0; i < cloud_position.size(); i++) {
+		int j = 0;
+		if (j == 0) {
+			cloud1.transform.translate = cloud_position[i];
+			cloud1.transform.rotate = rotation({ 0,0,1 }, cloud_orientation[i]);
+			draw(cloud1, scene);
+			j++;
+		}
+		if (j == 1) {
+			cloud2.transform.translate = cloud_position[i];
+			cloud2.transform.rotate = rotation({ 0,0,1 }, cloud_orientation[i]);
+			draw(cloud2, scene);
+			j++;
+		}
+		if (j == 2) {
+			cloud3.transform.translate = cloud_position[i];
+			cloud3.transform.rotate = rotation({ 0,0,1 }, cloud_orientation[i]);
+			draw(cloud3, scene);
+			j++;
+		}
+		if (j == 3) {
+			cloud4.transform.translate = cloud_position[i];
+			cloud4.transform.rotate = rotation({ 0,0,1 }, cloud_orientation[i]);
+			draw(cloud4, scene);
+			j=0;
+		}
+	}
+	ocean.transform.translate = { 0,0,0 };
+	draw(ocean, scene);	
+
+	wall.transform.rotate = rotation({ 0,0,1 },0);
+	wall.transform.translate = { 0,0,0 };
+	draw(wall, scene);
+
+	wall.transform.translate = { 0.0f,-taille_terrain,0 };
+	draw(wall, scene);
+
+	wall.transform.rotate = rotation({ 0,0,1 }, 0.5f* M_PI);
+	wall.transform.translate = { 0.0f,0,0 };
+	draw(wall, scene);
+	wall.transform.translate = { taille_terrain,0,0 };
+	draw(wall, scene);
+	
+	draw(ring, scene);
+
 	
 }
 
